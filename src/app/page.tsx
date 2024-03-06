765fa3215e65388996c16d397be400c43f4a6d34
temptedwithouta <danielsulistio125@gmail.com>
2024-03-06 13:14:39 +0700

    Benerin config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,67 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Header from "@/layouts/headers/header";
+import HeroBanner from "./components/hero-banners/hero-banner";
+import CategorySection from "./components/category/category-section";
+import FeatureOne from "./components/features/feature-one";
+import HowItWorks from "./components/how-it-works/how-it-works";
+import ExpertsOne from "./components/experts/experts-one";
+import FeedbackOne from "./components/feedBacks/feedback-one";
+import FeatureTwo from "./components/features/feature-two";
+import BlogOne from "./components/blogs/blog-one";
+import FancyBanner from "./components/fancy-banner/fancy-banner";
+import JobPortalIntro from "./components/job-portal-intro/job-portal-intro";
+import FooterOne from "@/layouts/footers/footer-one";
+import Wrapper from "@/layouts/wrapper";
 
 export default function Home() {
-  const hello = 'Hello'; // If using doublequote ("") will change to singlequote ('')
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Wrapper>
+      {/* header start */}
+      <Header />
+      {/* header end */}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* hero banner start */}
+      <HeroBanner />
+      {/* hero banner end */}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {/* category section start */}
+      <CategorySection/>
+      {/* category section end */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      {/* feature one start */}
+      <FeatureOne/>
+      {/* feature one end */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+      {/* how works start */}
+      <HowItWorks/>
+      {/* how works end */}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* expert one section start */}
+      <ExpertsOne/>
+      {/* expert one section end */}
+
+      {/* feedback one start */}
+      <FeedbackOne/>
+      {/* feedback one end */}
+
+      {/* text feature two start */}
+      <FeatureTwo/>
+      {/* text feature two end */}
+
+      {/* blog section one start */}
+      <BlogOne/>
+      {/* blog section one end */}
+
+      {/* fancy banner start */}
+      <FancyBanner/>
+      {/* fancy banner end */}
+
+      {/* job portal intro start */}
+      <JobPortalIntro/>
+      {/* job portal intro end */}
+
+      {/* footer start */}
+      <FooterOne/>
+      {/* footer end */}
+    </Wrapper>
   );
 }
