d082ce66f012dedaf8827a910397eadab6609d09
raxle789 <isqdifatih182@gmail.com>
2024-05-06 09:50:30 +0700

    feat: membuat applicant item & memperbaiki halaman assessment

--- src/app/components/dashboard/employ/job-applicant.tsx
+++ src/app/components/dashboard/employ/job-applicant.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import EmployJobItem from './job-item';
 import EmployShortSelect from './short-select';
@@ -11,6 +11,10 @@ import candidate_data from '@/data/candidate-data';
 import CandidateAssessmentItem from './candidate-assessment-item';
 import { useAppDispatch } from '@/redux/hook';
 import { setApplicantStep } from '@/redux/features/applicantStepSlice';
+import type { CheckboxProps } from 'antd';
+import { Checkbox, Popover } from 'antd';
+import ActionCheckboxPipeline from '../../common/popup/action-checkbox-pipeline';
+import ApplicantsItems from './applicants-item';
 
 const JobApplicantArea = () => {
   const dispatch = useAppDispatch();
@@ -30,7 +34,45 @@ const JobApplicantArea = () => {
     }
     router.replace(`${pathname}?${params.toString()}`);
   }, 300);
-  const candidate_items = candidate_data.slice(0, 5);
+  const candidate_items = candidate_data.slice(0, 10);
+
+  const initialCheckboxState = candidate_data?.reduce(
+    (acc: { [key: string]: boolean }, _: any, index: string) => {
+      return {
+        ...acc,
+        [index]: false,
+      };
+    },
+    {},
+  );
+  const [checkboxAllValue, setCheckboxAllValue] = useState(false);
+  const [checkbox, setCheckbox] = useState<{ [key: string]: boolean }>(
+    initialCheckboxState,
+  );
+  const [popOverState, setPopOverState] = useState(false);
+  const onChangeCheckboxAll: CheckboxProps['onChange'] = (e) => {
+    const checked = e.target.checked;
+    const updatedCheckbox: { [key: string]: boolean } = {};
+
+    Object.keys(checkbox).forEach((key: string) => {
+      updatedCheckbox[key] = checked;
+    });
+
+    setCheckbox(updatedCheckbox);
+    setCheckboxAllValue(checked);
+  };
+
+  useEffect(() => {
+    const countTrueValues = Object.values(checkbox).reduce(
+      (acc, curr) => acc + (curr ? 1 : 0),
+      0,
+    );
+    if (countTrueValues > 1) {
+      setPopOverState(true);
+    } else {
+      setPopOverState(false);
+    }
+  }, [checkbox]);
 
   return (
     <>
@@ -126,9 +168,29 @@ const JobApplicantArea = () => {
         </form>
       </div>
 
+      <div className="card-checkbox">
+        <Popover
+          content={<ActionCheckboxPipeline />}
+          trigger="click"
+          open={popOverState}
+          placement="right"
+        >
+          <Checkbox
+            onChange={onChangeCheckboxAll}
+            checked={checkboxAllValue}
+          ></Checkbox>
+        </Popover>
+      </div>
       <div className="wrapper">
         {candidate_items.map((item) => (
-          <CandidateAssessmentItem key={item.id} item={item} />
+          <ApplicantsItems
+            key={item.id}
+            item={item}
+            checkboxState={checkbox}
+            checkboxAllValue={checkboxAllValue}
+            setCheckbox={setCheckbox}
+            setCheckboxAllValue={setCheckboxAllValue}
+          />
         ))}
       </div>
 
