c7e9fd756bd7e5a2b36a9b17e97c647072215774
raxle789 <isqdifatih182@gmail.com>
2024-05-08 10:53:13 +0700

    fix: merubah applied job, partner slider, job detail, dll

--- src/app/components/dashboard/employ/job-applicant.tsx
+++ src/app/components/dashboard/employ/job-applicant.tsx
@@ -1,23 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
-import EmployJobItem from './job-item';
-import EmployShortSelect from './short-select';
 import ActionDropdownJobDetails from '../candidate/action-dropdown-job-details';
-import Image from 'next/image';
-import search from '@/assets/dashboard/images/icon/icon_10.svg';
+import SearchBar from '@/ui/search-bar';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
-import candidate_data from '@/data/candidate-data';
-import CandidateAssessmentItem from './candidate-assessment-item';
-import { useAppDispatch } from '@/redux/hook';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { setApplicantStep } from '@/redux/features/applicantStepSlice';
-import type { CheckboxProps } from 'antd';
-import { Checkbox, Popover } from 'antd';
-import ActionCheckboxPipeline from '../../common/popup/action-checkbox-pipeline';
-import ApplicantsItems from './applicants-item';
-import ApplicantMenu from './applicant-menu';
-import { Tabs } from 'antd';
-import type { TabsProps } from 'antd';
+import ApplicantArea from './applicant-area';
+import AssessmentArea from './assessment-area';
 
 const JobApplicantArea = () => {
   const dispatch = useAppDispatch();
@@ -25,7 +15,7 @@ const JobApplicantArea = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  dispatch(setApplicantStep({ currentStep: 1 }));
+  // dispatch(setApplicantStep({ currentStep: 1 }));
 
   const handleJobFpkSearch = useDebouncedCallback((value) => {
     const params = new URLSearchParams(searchParams);
@@ -38,54 +28,7 @@ const JobApplicantArea = () => {
     router.replace(`${pathname}?${params.toString()}`);
   }, 300);
 
-  const [keyState, setKeyState] = useState('1');
-
-  const onChange = (key: string) => {
-    setKeyState(key);
-  };
-
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: 'Applicant 200',
-    },
-    {
-      key: '2',
-      label: 'Shortlisted 200',
-    },
-    {
-      key: '3',
-      label: 'Talent Pool 200',
-    },
-    {
-      key: '4',
-      label: 'Assessment 200',
-    },
-    {
-      key: '5',
-      label: 'Interview 200',
-    },
-    {
-      key: '6',
-      label: 'Ref Check 200',
-    },
-    {
-      key: '7',
-      label: 'Offering 200',
-    },
-    {
-      key: '8',
-      label: 'MCU 200',
-    },
-    {
-      key: '9',
-      label: 'Agreement 200',
-    },
-    {
-      key: '10',
-      label: 'Boarding 200',
-    },
-  ];
+  const partState = useAppSelector((state) => state.applicantStep.step);
   return (
     <>
       <div className="d-sm-flex align-items-start justify-content-between mb-10 lg-mb-30">
@@ -109,80 +52,101 @@ const JobApplicantArea = () => {
           </div>
         </div>
       </div>
-      {/* <div className="nav-bar-responsive d-flex align-items-center justify-content-center mb-40 pb-3 overflow-auto"> */}
-      {/* <Link href="#" className="d-flex flex-column align-items-center me-4">
+      <div className="nav-bar-responsive d-flex align-items-center justify-content-center mb-40 pb-3 overflow-auto">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 1 }))}
+        >
           <span>0</span>
           <span>Applicants</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 2 }))}
+        >
           <span>0</span>
           <span>Shortlisted</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 3 }))}
+        >
           <span>0</span>
           <span className="text-center">Talent Pool</span>
         </Link>
         <Link
-          href="/dashboard/ta/preview-page/assessment"
-          onClick={() =>
-            dispatch(setApplicantStep({ currentStep: 'assessment' }))
-          }
+          href=""
           className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 4 }))}
         >
           <span>3</span>
           <span>Assessment</span>
         </Link>
         <Link
-          href="/dashboard/ta/preview-page/interview"
-          onClick={() =>
-            dispatch(setApplicantStep({ currentStep: 'interview' }))
-          }
+          href=""
           className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 5 }))}
         >
           <span>0</span>
           <span>Interview</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 6 }))}
+        >
           <span>0</span>
           <span className="text-center">Ref Check</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 7 }))}
+        >
           <span>0</span>
           <span>Offering</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 8 }))}
+        >
           <span>0</span>
           <span>MCU</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center me-4">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center me-4"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 9 }))}
+        >
           <span>0</span>
           <span>Agreement</span>
         </Link>
-        <Link href="#" className="d-flex flex-column align-items-center">
+        <Link
+          href=""
+          className="d-flex flex-column align-items-center"
+          onClick={() => dispatch(setApplicantStep({ currentStep: 10 }))}
+        >
           <span>0</span>
           <span>Boarding</span>
-        </Link> */}
-      {/* </div> */}
-
-      <div className="d-flex justify-content-end mb-40">
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          className="search-form form-fpk"
-        >
-          <input
-            type="text"
-            placeholder="Search here.."
-            onChange={(e) => handleJobFpkSearch(e.target.value)}
-            defaultValue={searchParams.get('query')?.toString()}
-          />
-          <button type="submit">
-            <Image src={search} alt="search" className="lazy-img m-auto" />
-          </button>
-        </form>
+        </Link>
       </div>
 
-      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
-      {keyState === '1' && <ApplicantMenu />}
+      <div className="d-flex justify-content-between align-items-center mb-40">
+        <div>
+          {partState === 1 && <h4 className="sub-main-title">Applicant</h4>}
+          {partState === 4 && <h4 className="sub-main-title">Assessment</h4>}
+          {partState === 5 && <h4 className="sub-main-title">Interview</h4>}
+        </div>
+        <SearchBar />
+      </div>
+
+      {partState === 1 && <ApplicantArea />}
+      {partState === 4 && <AssessmentArea />}
+      {/* {partState === 5 && <ApplicantArea />} */}
     </>
   );
 };
